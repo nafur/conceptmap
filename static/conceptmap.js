2e51a36c39a03b96bfaf341daa4ee3e4de2c458c
Gereon Kremer <gereon.kremer@rwth-aachen.de>
2016-01-30 12:21:41 +0100

    reorganizing action handling.

--- static/conceptmap.js
+++ static/conceptmap.js
@@ -1,9 +1,10 @@
 jsPlumb.ready(function () {
-	
+
+	// Action, Time, SourceName, SourceID, TargetName, TargetID, Data1, Data2, Data3
 	var past = new Array();
 	var future = new Array();
 	var baseTime = $.now();
-	
+
     function stateName(s) {
     	return $("#" + s + "-name").html();
     }
@@ -15,17 +16,57 @@ jsPlumb.ready(function () {
 		if (d3 !== null && d3 !== undefined) data.push(d3);
 		past.push(data);
 	}
-	
+	function action(s,t,d1,d2,d3) {
+		var t = $.now() - baseTime;
+		var data = [s, t, stateName(c.sourceId), c.sourceId, stateName(c.targetId), c.targetId];
+		if (d1 !== null && d1 !== undefined) data.push(d1);
+		if (d2 !== null && d2 !== undefined) data.push(d2);
+		if (d3 !== null && d3 !== undefined) data.push(d3);
+		return data;
+	}
+
 	function checkTime() {
 		var curTime = $.now();
 		var diff = Math.floor((curTime - baseTime) / 1000);
 		var min = Math.floor(diff / 60);
 		var sek = Math.floor(diff % 60);
-		
+
 		if (min == 10 && sek == 0) alert("Bitte kommen Sie zum Ende.");
-		
+
 		if (sek < 10) sek = '0' + sek;
-		$('#zeit').html(min + ':' + sek);		
+		$('#zeit').html(min + ':' + sek);
+	}
+
+	funciton logAction(action) {
+		console.log("Did " + action);
+		past.push(action);
+	}
+	function doAction(action) {
+		console.log("Doing " + action);
+		if (action[0] == "connect") {
+			instance.connect({source: action[3], target: action[5]});
+		} else if (action[0] == "detach") {
+			var conn = instance.getConnections({source: action[3], target: action[5]})[0];
+			jsPlumb.detach(conn);
+		} else if (action[0] == "rename") {
+			var conn = instance.getConnections({source: action[3], target: action[5]})[0];
+			conn.getOverlay("label").setLabel(action[7]);
+		}
+		logAction(action);
+	}
+
+	function undoAction(action) {
+		console.log("Undoing " + action);
+		future.push(action);
+		if (action[0] == "connect") {
+			var c = instance.getConnections({source: action[3], target: action[5]})[0];
+			jsPlumb.detach(c);
+		} else if (action[0] == "detach") {
+			var c = instance.connect({source: action[3], target: action[5]});
+		} else if (action[0] == "rename") {
+			var c = instance.getConnections({source: action[3], target: action[5]})[0];
+			c.getOverlay("label").setLabel(action[6]);
+		}
 	}
 
     // setup some defaults for jsPlumb.
@@ -38,8 +79,7 @@ jsPlumb.ready(function () {
             [ "Label", { label: "<span class=\"glyphicon glyphicon-remove\"></span>", id: "delete", cssClass: "aLabel", location: 0.25,
             	events:{click:function(overlay,event){
             		var c = overlay.component;
-            		log("detach", c, c.getOverlay("label").getLabel());
-            		jsPlumb.detach(c);
+					doAction(action("detach", c, c.getOverlay("label").getLabel()));
 				}}
             }]
         ],
@@ -48,59 +88,19 @@ jsPlumb.ready(function () {
 
     window.jsp = instance;
     var windows = jsPlumb.getSelector(".conceptmap .w");
-    
-    function layout() {
-		var instance = window.jsp;
-		var options = {name: "springy", repulsion: 10000, animate: false, padding: 100, infinite: false, maxSimulationTime: 1000, random: true, fit: true, boundingBox: {x1: 0,y1: 0,w: $("#conceptmap").width()-100,h: $("#conceptmap").height()-100}};
-//		var options = {name: "cola", maxSimulationTime: 1000, randomize: true, fit: false, boundingBox: {x1: 0,y1: 0,w: $("#conceptmap").width() - 150,h: $("#conceptmap").height() - 150}};
-		var cy = cytoscape({headless: true, layout: options});
-		$('.w').each(function(i,obj){ cy.add({ group: "nodes", data: {id: obj.id}, position: {x: i*20, y: i*20} }); });
-		var cons = instance.getAllConnections();
-		$.each(cons, function(i,obj){ cy.add({ group: "edges", data: {source: obj.sourceId, target: obj.targetId} }); });
-		var layout = cy.makeLayout(options);
-		layout.run();
-		setTimeout(function(){ 
-			layout.stop();
-			$.each(cy.json().elements.nodes, function(i,obj){ var o = $("#" + obj.data.id); o.css("left",obj.position.x); o.css("top", obj.position.y); o.simulate("drag", {moves: 1, dx: 1, dy: 0}); });
-		}, 1000);
-	}
-	$("#layout").click(layout);
+
 
 	function backward() {
 		if (past.length == 0) return;
-		var a = past.pop();
-		future.push(a);
-		if (a[0] == "connect") {
-			var c = instance.getConnections({source: a[3], target: a[5]})[0];
-			jsPlumb.detach(c);
-		} else if (a[0] == "detach") {
-			var c = instance.connect({source: a[3], target: a[5]});
-			past.pop();
-		} else if (a[0] == "rename") {
-			var c = instance.getConnections({source: a[3], target: a[5]})[0];
-			c.getOverlay("label").setLabel(a[6]);
-		}
-//		alert(past + " -- " + future);
+		undoAction(past.pop());
 	}
 	$("#backward").click(backward);
 	function forward() {
 		if (future.length == 0) return;
-		var c = future.pop();
-		past.push(c);
-		if (c[0] == "connect") {
-			instance.connect({source: c[3], target: c[5]});
-			past.pop();
-		} else if (c[0] == "detach") {
-			var conn = instance.getConnections({source: c[3], target: c[5]})[0];
-			jsPlumb.detach(conn);
-		} else if (c[0] == "rename") {
-			var conn = instance.getConnections({source: c[3], target: c[5]})[0];
-			conn.getOverlay("label").setLabel(c[7]);
-		}
-//		alert(past + " -- " + future);
+		doAction(future.pop());
 	}
 	$("#forward").click(forward);
-	
+
 	function findEmptyLabels() {
 		var res = [];
 		var conns = instance.getConnections();
@@ -111,7 +111,7 @@ jsPlumb.ready(function () {
 		}
 		return res;
 	}
-	
+
 	function finish() {
 		if (session != "") {
 			var empty = findEmptyLabels();
@@ -138,7 +138,7 @@ jsPlumb.ready(function () {
 		}
 	}
 	$("#finish").click(finish);
-	
+
 	function screenshot() {
 		html2canvas(document.body, {
 			logging: true,
@@ -155,7 +155,7 @@ jsPlumb.ready(function () {
 
     // initialise draggable elements.
     instance.draggable(windows, {containment: "parent", handle: ".w-drag"});
-    
+
     function triggerClick(elem) {
     	if (elem.click) elem.click();
     	else {
@@ -167,15 +167,15 @@ jsPlumb.ready(function () {
 
     // On create connection
     instance.bind("connection", function (info) {
-    	log("connect", info);
+		logAction(action("connect", info));
 		instance.recalculateOffsets("conceptmap");
 		instance.repaintEverything();
         info.connection.getOverlay("label").setLabel("");
-        $(".edit").editable(function(value,settings,arg){ 
+        $(".edit").editable(function(value,settings,arg){
         	if (arg != value) {
-	        	log("rename", info, arg, value);
+				logAction(action(rename, info, arg, value));
 			}
-        	return (value); 
+        	return (value);
 		},{
 			submitdata: function(val,settings) { return {original: this.revert}; },
 			onblur: "submit"
@@ -183,7 +183,7 @@ jsPlumb.ready(function () {
 		var label = info.connection.getOverlay("label").canvas;
     	triggerClick(label);
     });
-    
+
     function exportAsMap() {
     	var graph = {};
     	for (var i = 0; i < past.length; i++) {
@@ -194,7 +194,7 @@ jsPlumb.ready(function () {
     	}
     	return graph;
     }
-    
+
     // Send data to server every few seconds
     function sendData() {
     	if (session != "") {
@@ -237,12 +237,31 @@ jsPlumb.ready(function () {
         });
 
     });
-    
+
     //while (restore_data.length > 0) {
     //	future.push(restore_data.pop());
     //}
     if ($("#zeit").length) {
 		setInterval(checkTime, 500);
     }
-});
 
+	/*
+	// layouting
+    function layout() {
+		var instance = window.jsp;
+		var options = {name: "springy", repulsion: 10000, animate: false, padding: 100, infinite: false, maxSimulationTime: 1000, random: true, fit: true, boundingBox: {x1: 0,y1: 0,w: $("#conceptmap").width()-100,h: $("#conceptmap").height()-100}};
+//		var options = {name: "cola", maxSimulationTime: 1000, randomize: true, fit: false, boundingBox: {x1: 0,y1: 0,w: $("#conceptmap").width() - 150,h: $("#conceptmap").height() - 150}};
+		var cy = cytoscape({headless: true, layout: options});
+		$('.w').each(function(i,obj){ cy.add({ group: "nodes", data: {id: obj.id}, position: {x: i*20, y: i*20} }); });
+		var cons = instance.getAllConnections();
+		$.each(cons, function(i,obj){ cy.add({ group: "edges", data: {source: obj.sourceId, target: obj.targetId} }); });
+		var layout = cy.makeLayout(options);
+		layout.run();
+		setTimeout(function(){
+			layout.stop();
+			$.each(cy.json().elements.nodes, function(i,obj){ var o = $("#" + obj.data.id); o.css("left",obj.position.x); o.css("top", obj.position.y); o.simulate("drag", {moves: 1, dx: 1, dy: 0}); });
+		}, 1000);
+	}
+	$("#layout").click(layout);
+	*/
+});
